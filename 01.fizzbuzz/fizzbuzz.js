50c281db8ba37939df901e3ae5032fbe160a2df8
nakatakut2 <nakatakut2@gamil.com>
2023-06-28 09:48:42 +0900

    constで変数宣言、厳密等価演算子で値の比較

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,12 +1,12 @@
-let number = [
+const numbers = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
-for (let x of number) {
-  if (x % 3 == 0 && x % 5 == 0) {
+for (const x of numbers) {
+  if (x % 3 === 0 && x % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (x % 5 == 0) {
+  } else if (x % 5 === 0) {
     console.log("Buzz");
-  } else if (x % 3 == 0) {
+  } else if (x % 3 === 0) {
     console.log("Fizz");
   } else {
     console.log(x);
