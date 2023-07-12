1bf1e6ef7030b15ab157e4e2e2519f8e2f50318b
nakatakut2 <nakatakut2@gamil.com>
2023-07-12 15:09:12 +0900

    末日の改行、式展開、padStart & padEnd

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -10,15 +10,15 @@ let month = argv["m"] || dayjs().format("M");
 let startDate = dayjs(new Date(year, month - 1, 1));
 let lastDate = dayjs(new Date(year, month, 0));
 
-console.log("      " + month + "月 " + year);
+console.log(`${month}月 ${year}`.padStart(13));
 console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(startDate.day()));
 
 for (let i = 1; i <= lastDate.date(); i++) {
-  let formattedDate = i < 10 ? " " + i + " " : i + " ";
+  let formattedDate = String(i).padStart(2).padEnd(3);
   process.stdout.write(formattedDate);
 
-  if (startDate.date(i).day() === 6) {
+  if (startDate.date(i).day() === 6 || i === lastDate.date()) {
     process.stdout.write("\n");
   }
 }
